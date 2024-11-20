3e3a98f1b7f4f60c342fd11fa1869533fce0bd6a
MIrzoyev1 <ali.mirzoyev.amr2@gmail.com>
2024-11-20 12:12:36 +0000

    fix

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key:
-        a_dictionary[key] = value
-    if not key:
-        a_dictionary[key] = value
+    a_dictionary[key] = value
 
