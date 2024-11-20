dec9f307aeea6ac82c6123d24534bc44557f9064
MIrzoyev1 <ali.mirzoyev.amr2@gmail.com>
2024-11-20 10:50:44 +0000

    fix

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -2,7 +2,7 @@
 import sys
 
 if __name__ == "__main__":
-    num_args = len(sys.argv) - 1  # Exclude the script name from the argument count
+    num_args = len(sys.argv) - 1
     if num_args == 0:
         print("0 arguments.")
     elif num_args == 1:
@@ -12,5 +12,3 @@ if __name__ == "__main__":
 
     for i in range(1, num_args + 1):
         print(f"{i}: {sys.argv[i]}")
-
-
