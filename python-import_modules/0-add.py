65c6ecc88b8b7bed5997c4fc2509138170a26321
MIrzoyev1 <ali.mirzoyev.amr2@gmail.com>
2024-11-21 12:10:05 +0400

    Update 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,7 @@
-#!/usr/bin/python:wq
-3
-if __name__ == "__main__":
-    from add_0 import add
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    occurrences = lazy_int(number_of_occurrences("0-add.py", "add_0", case_sensitive=True))
-    if occurrences != 1:
-        print("Error: number_of_occurrences did not return the correct count.")
+#!/usr/bin/python3
+from add_0 import add
 
+a = 1
+b = 2
+
+print("{} + {} = {}".format(a, b, add(a, b)))
